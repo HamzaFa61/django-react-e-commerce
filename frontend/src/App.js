138b584e0e97f9b04d3756e92046e66dcfd925ae
Hamza Farooq <farooqhamza61@gmail.com>
2023-10-25 16:34:30 +0500

    Add Cart Functionality Complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,7 +18,7 @@ function App() {
 				<Routes>
 					<Route path="/" element={<HomeScreen/>} exact />
 					<Route path="/product/:id" element={<ProductScreen />} />
-					<Route path="/cart/:id?" element={<CartScreen />} />
+					<Route path="/cart/:productId?" element={<CartScreen />} />
 				</Routes>
 				<Footer />
 			</Router>
--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,11 +1,11 @@
 import {
-    ADD_TO_CART,
-    REMOVE_ITEM,
+    CART_ADD_ITEM,
+    CART_REMOVE_ITEM,
 } from "../constants/cartConstants";
 
 export const cartReducer = (state = { cartItems: [] }, action) => {
     switch (action.type) {
-        case ADD_TO_CART:
+        case CART_ADD_ITEM:
             const item = action.payload;
 
             const existItem = state.cartItems.find((x) => x.product === item.product);
@@ -23,7 +23,7 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
                     cartItems: [...state.cartItems, item],
                 };
             }
-        case REMOVE_ITEM:
+        case CART_REMOVE_ITEM:
             return {
                 ...state,
                 cartItems: state.cartItems.filter((x) => x.product !== action.payload),
