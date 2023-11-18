718ec07fbe9b8badbd64a3f6735ef9456f380a69
Hamza Farooq <farooqhamza61@gmail.com>
2023-11-18 19:39:37 +0500

    weird bug fix

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -5,8 +5,8 @@ from base.views import user_views
 urlpatterns = [
     path('login/', user_views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('profile/', user_views.get_user_profile, name='user-profile'),
-    path('<str:pk>/', user_views.get_user_by_id, name='user-by-id'),
-    path('profile/update/', user_views.update_user_profile, name='user-profile-update'),
     path('', user_views.get_users, name='users'),
     path('register/', user_views.register_user, name='register'),
+    path('<str:pk>/', user_views.get_user_by_id, name='user-by-id'),
+    path('profile/update/', user_views.update_user_profile, name='user-profile-update'),
 ]
